2701a27c0450c10139173980b615bf1339ac55d3
Eduardo <ceduardolima2000@gmail.com>
2024-05-07 16:26:20 -0300

    Retornando BadRequest com os erros ao criar uma conta inválida

--- metas_fin/lib/metas_fin_web/erros/error_handler.ex
+++ metas_fin/lib/metas_fin_web/erros/error_handler.ex
@@ -5,3 +5,8 @@ end
 defmodule MetasFinWeb.Errors.ErrorHandler.Forbidden do
   defexception message: "Accesso negado", plug_status: 403
 end
+
+
+defmodule MetasFinWeb.Errors.ErrorHandler.BadRequest do
+  defexception message: "Bad request", plug_status: 404
+end
